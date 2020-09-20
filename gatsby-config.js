0c070a6f4176ed1a6dfd76773c2daafb321f9eea
Alex Isayyid <6026917+aisayyid@users.noreply.github.com>
2020-09-19 21:34:21 -0400

    changed titles and manifest data.

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,8 +22,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Simplefolio`,
-        short_name: `Simplefolio`,
+        name: `Alex Isayyid's Portfolio`,
+        short_name: `AI Portfolio`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#02aab0`,
